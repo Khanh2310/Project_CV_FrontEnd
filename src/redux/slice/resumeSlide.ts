cf93eaaf95385e38077bc75a6da1b826e688bbb3
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-04-15 11:11:35 +0700

    fix UI

--- src/redux/slice/resumeSlide.ts
+++ src/redux/slice/resumeSlide.ts
@@ -34,28 +34,19 @@ const initialState: IState = {
 export const resumeSlide = createSlice({
   name: 'resume',
   initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    // Use the PayloadAction type to declare the contents of `action.payload`
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setActiveMenu: (state, action) => {
-      // state.activeMenu = action.payload;
-    },
+    setActiveMenu: (state, action) => {},
   },
   extraReducers: (builder) => {
-    // Add reducers for additional action types here, and handle loading state as needed
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     builder.addCase(fetchResume.pending, (state, action) => {
       state.isFetching = true;
-      // Add user to the state array
-      // state.courseOrder = action.payload;
     });
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     builder.addCase(fetchResume.rejected, (state, action) => {
       state.isFetching = false;
-      // Add user to the state array
-      // state.courseOrder = action.payload;
     });
 
     builder.addCase(fetchResume.fulfilled, (state, action) => {
@@ -64,9 +55,6 @@ export const resumeSlide = createSlice({
         state.meta = action.payload.data.meta;
         state.result = action.payload.data.result;
       }
-      // Add user to the state array
-
-      // state.courseOrder = action.payload;
     });
   },
 });
